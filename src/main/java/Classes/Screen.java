d00bef77d948fe9a324909dd650b23765a560a2c
Clodomir Junior <clodomirjunior171@gmail.com>
2023-05-24 22:45:49 -0300

    Primeiro comit teste

---
+++ src/main/java/Classes/Screen.java
@@ -0,0 +1,14 @@
+package Classes;
+
+import javax.swing.*;
+
+public class Screen {
+    public Screen(){
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setSize(800, 500);
+        jFrame.setTitle("Bliblioteca Mundo Encantado");
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+
+    }
+}
