a32492d9319738cf101f7d505160b7abd6e6db24
Clodomir Junior <clodomirjunior171@gmail.com>
2023-05-25 22:44:48 -0300

    Changes by lucasvilar

--- src/main/java/Classes/Pessoa.java
+++ src/main/java/Classes/Pessoa.java
@@ -3,5 +3,5 @@ package Classes;
 public class Pessoa {
     private String nome;
     private int idade;
-
+    private int cpf;
 }
--- src/main/java/Classes/Usuario.java
+++ src/main/java/Classes/Usuario.java
@@ -1,5 +1,5 @@
 package Classes;
 
-public class Usuario { ;
+public class Usuario {
 
 }
